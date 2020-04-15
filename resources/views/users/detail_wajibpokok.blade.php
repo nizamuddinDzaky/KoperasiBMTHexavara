f747999a5a63be52e9509ca45c3676259344b182
Muhammad Alibiyanto <gocode2019@gmail.com>
2020-04-15 20:19:27 +0700

    Fix dashboard anggota

--- resources/views/users/detail_wajibpokok.blade.php
+++ resources/views/users/detail_wajibpokok.blade.php
@@ -36,8 +36,7 @@
                             <th class="text-center" data-sortable="true">Ke Rekening</th>
                             <th class="text-center" data-sortable="true">Jenis Transaksi</th>
                             <th class="text-center" data-sortable="true">Jumlah</th>
-                            <th class="text-center" data-sortable="true">Saldo Awal</th>
-                            <th class="text-center" data-sortable="true">Saldo Akhir</th>
+                            <th class="text-center" data-sortable="true">Saldo</th>
                             {{--<th>Actions</th>--}}
                             <th></th>
                             </thead>
@@ -50,7 +49,7 @@
                                         <td class="text-left text-uppercase">[ {{ $usr['untuk_rekening'] }} ]</td>
                                         <td class="text-center text-uppercase">{{$usr['status'] }}</td>
                                         <td class="text-right">{{ number_format(json_decode($usr['transaksi'], true)['jumlah'],2) }}</td>
-                                        <td class="text-right">{{ number_format(json_decode($usr['transaksi'],true)['saldo_awal'],2) }}</td>
+                                        {{-- <td class="text-right">{{ number_format(json_decode($usr['transaksi'],true)['saldo_awal'],2) }}</td> --}}
                                         <td class="text-right">{{ number_format(json_decode($usr['transaksi'],true)['saldo_akhir'],2) }}</td>
                                         <td></td>
                                     </tr>
